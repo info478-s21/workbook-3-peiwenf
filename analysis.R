6cbcdc451f1024c56cbb8ad09115d020486327ac
Cici <peiwenfeng0825@gmail.com>
2021-04-30 01:53:27 -0700

    finished

--- analysis.R
+++ analysis.R
@@ -1,4 +1,6 @@
 # Analysis
+#Cici Feng
+#wb-03
 
 # Set up - make sure to set your working directory using RStudio
 library(tidyr)
@@ -6,17 +8,43 @@ library(dplyr)
 library(ggplot2)
 
 # Create the `charts/` directory (you can do this from R!)
+dir.create("charts", showWarnings = FALSE)
 
 # Load prepped data
+health_data <- read.csv("./data/prepped/all_data.csv")
 
 # Are HALE and life expectancy correlated?
 # - Plot 2016 life expectancy against 2016 HALE. Save the graph to `charts/`
 # - Compute the correlation between 2016 life expectancy against 2016 HALE
+data_2016 <- health_data %>% 
+  filter(year == 2016)
+
+ggplot(data_2016) + 
+  geom_point(mapping = aes(x = le, y = hale)) +
+  labs(title = "Life Expectancy vs HALE",
+       x = "Life Expectancy",
+       y = "HALE")
+
+ggsave("charts/le_hale_graph.png")
+
+cor(data_2016$hale, data_2016$le)
+#Since the correlation is 0.9957 which is close to 1,
+#they are strongly correlated.
 
 # Are HALE and DALYs correlated?
 # - Plot 2016 HALE against 2016 DALYs. Save the graph to `charts/`
 # - Compute the correlation between 2016 HALE and DALYs
+ggplot(data_2016) + 
+  geom_point(mapping = aes(x = dalys, y = hale)) +
+  labs(title = "DALYS vs HALE",
+       x = "DALY",
+       y = "HALE")
 
+ggsave("charts/daly_hale_graph.png")
+
+cor(data_2016$hale, data_2016$dalys)
+#Since the correlation is 0.9859, HALE and DALYS is not 
+#as strongly correlated as HALE and Life Expectancy.
 
 # As people live longer, do they live healthier lives 
 # (i.e., is a smaller fraction of life spent in poor health)?
@@ -26,11 +54,31 @@ library(ggplot2)
 # This will create `hale_2016`, `hale_1990`, `le_2016`, etc.
 # To do this, I suggest that you use the `pivot` function in the new
 # tidyverse release:https://tidyr.tidyverse.org/articles/pivot.html#wider
-
+data_wide <- health_data %>% 
+  pivot_wider(names_from = year, 
+              values_from = c(hale, le, dalys))
 
 # Create columns to store the change in life expectancy, and change in hale
-
+data_wide <- data_wide %>% 
+  mutate(hale_diff = hale_2016 - hale_1990,
+         le_diff = le_2016 - le_1990)
 
 # Plot the *change in hale* against the *change in life expectancy*
 # Add a 45 degree line (i.e., where x = y), and save the graph to `charts/`
 # What does this mean?!?! Put your interpretation below
+ggplot(data_wide) + 
+  geom_point(mapping = aes(x = le_diff, y = hale_diff)) +
+  labs(title = "Life Expectancy Difference vs HALE difference",
+       x = "Change in Life Expectancy",
+       y = "Change in HALE") +
+  geom_abline(intercept = 0, slope = 1) +
+  xlim(-15, 20) +
+  ylim(-15, 20)
+
+ggsave("charts/change.png")
+# And from the graph we can tell that since the curve is below the 45 degree line, 
+# the change in Life Expectancy changes faster than the change in HALE.
+
+
+
+
